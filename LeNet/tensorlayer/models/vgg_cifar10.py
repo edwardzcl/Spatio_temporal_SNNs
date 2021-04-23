81a98d7878f056fb0b6ccb3f0ee29ad8baacf6c8
Chenglong Zou <2829008362@qq.com>
2021-04-23 18:31:19 +0800

    update

--- LeNet/tensorlayer/models/vgg_cifar10.py
+++ LeNet/tensorlayer/models/vgg_cifar10.py
@@ -34,7 +34,7 @@ __all__ = [
 ]
 
 
-def VGG_CIFAR10(x_crop, y_, pretrained=False, end_with='fc1000', n_classes=1000, is_train=True, reuse=False, name=None):
+def VGG_CIFAR10(x_crop, y_, pretrained=False, k=1, B=2, end_with='fc1000', n_classes=1000, is_train=True, reuse=False, name=None):
     """Pre-trained MobileNetV1 model (static mode). Input shape [?, 224, 224, 3].
     To use pretrained model, input should be in BGR format and subtracted from ImageNet mean [103.939, 116.779, 123.68].
 
@@ -74,51 +74,51 @@ def VGG_CIFAR10(x_crop, y_, pretrained=False, end_with='fc1000', n_classes=1000,
          # the first layer is usually high-precision refer to the previous works, in order to transfer the continue-valued pixels to a spike-based representation.
          net = Conv2d(net, 64, (3, 3), (1, 1), padding='SAME', b_init=None, name='conv00')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn00')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          net = Quant_Conv2d(net, n_filter=64, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv0')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn0')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          net = Quant_Conv2d(net, n_filter=64, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv1')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn1')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          net = Quant_Conv2d(net, n_filter=64, filter_size=(2, 2), strides=(2, 2), padding='SAME', b_init=None, name='conv2')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn2')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          net = Quant_Conv2d(net, n_filter=128, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv3')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn3')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          net = Quant_Conv2d(net, n_filter=128, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv4')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn4')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          net = Quant_Conv2d(net, n_filter=128, filter_size=(2, 2), strides=(2, 2), padding='SAME', b_init=None, name='conv5')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn5')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          net = Quant_Conv2d(net, n_filter=256, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv6')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn6')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          net = Quant_Conv2d(net, n_filter=256, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv7')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn7')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          net = Quant_Conv2d(net, n_filter=256, filter_size=(2, 2), strides=(2, 2), padding='SAME', b_init=None, name='conv8')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn8')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          net = Quant_Conv2d(net, n_filter=512, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv9')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn9')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          net = Quant_Conv2d(net, n_filter=512, filter_size=(3, 3), strides=(1, 1), padding='VALID', b_init=None, name='conv10')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn10')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
 
   
--- LeNet/tensorlayer/models/vgg_cifar10_spike.py
+++ LeNet/tensorlayer/models/vgg_cifar10_spike.py
@@ -34,7 +34,7 @@ __all__ = [
 ]
 
 
-def VGG_CIFAR10_spike(x_crop, y_, pretrained=False, end_with='fc1000', n_classes=1000, is_train=True, reuse=False, name=None):
+def VGG_CIFAR10_spike(x_crop, y_, pretrained=False, k=1, B=2, end_with='fc1000', n_classes=1000, is_train=True, reuse=False, name=None):
     """Pre-trained MobileNetV1 model (static mode). Input shape [?, 224, 224, 3].
     To use pretrained model, input should be in BGR format and subtracted from ImageNet mean [103.939, 116.779, 123.68].
 
@@ -74,7 +74,7 @@ def VGG_CIFAR10_spike(x_crop, y_, pretrained=False, end_with='fc1000', n_classes
          # the first layer is usually high-precision refer to the previous works, in order to transfer the continue-valued pixels to a spike-based representation.
          net = Conv2d(net, 64, (3, 3), (1, 1), padding='SAME', b_init=None, name='conv00')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn00')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          spikes = tf.reduce_sum(net.outputs[:,0,0,:])*2*2*64
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,1,:])*3*2*64
@@ -102,7 +102,7 @@ def VGG_CIFAR10_spike(x_crop, y_, pretrained=False, end_with='fc1000', n_classes
 
          net = Quant_Conv2d(net, n_filter=64, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv0')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn0')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,0,:])*2*2*64
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,1,:])*3*2*64
@@ -130,13 +130,13 @@ def VGG_CIFAR10_spike(x_crop, y_, pretrained=False, end_with='fc1000', n_classes
 
          net = Quant_Conv2d(net, n_filter=64, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv1')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn1')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          spikes = spikes + tf.reduce_sum(net.outputs)*1*1*64
 
          net = Quant_Conv2d(net, n_filter=64, filter_size=(2, 2), strides=(2, 2), padding='SAME', b_init=None, name='conv2')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn2')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,0,:])*2*2*128
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,1,:])*3*2*128
@@ -164,7 +164,7 @@ def VGG_CIFAR10_spike(x_crop, y_, pretrained=False, end_with='fc1000', n_classes
 
          net = Quant_Conv2d(net, n_filter=128, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv3')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn3')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,0,:])*2*2*128
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,1,:])*3*2*128
@@ -192,13 +192,13 @@ def VGG_CIFAR10_spike(x_crop, y_, pretrained=False, end_with='fc1000', n_classes
 
          net = Quant_Conv2d(net, n_filter=128, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv4')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn4')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          spikes = spikes + tf.reduce_sum(net.outputs)*1*1*128
 
          net = Quant_Conv2d(net, n_filter=128, filter_size=(2, 2), strides=(2, 2), padding='SAME', b_init=None, name='conv5')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn5')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,0,:])*2*2*256
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,1,:])*3*2*256
@@ -227,7 +227,7 @@ def VGG_CIFAR10_spike(x_crop, y_, pretrained=False, end_with='fc1000', n_classes
 
          net = Quant_Conv2d(net, n_filter=256, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv6')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn6')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,0,:])*2*2*256
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,1,:])*3*2*256
@@ -254,14 +254,14 @@ def VGG_CIFAR10_spike(x_crop, y_, pretrained=False, end_with='fc1000', n_classes
 
          net = Quant_Conv2d(net, n_filter=256, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv7')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn7')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          spikes = spikes + tf.reduce_sum(net.outputs)*1*1*256
 
 
          net = Quant_Conv2d(net, n_filter=256, filter_size=(2, 2), strides=(2, 2), padding='SAME', b_init=None, name='conv8')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn8')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,0,:])*2*2*512
          spikes = spikes + tf.reduce_sum(net.outputs[:,0,1,:])*3*2*512
@@ -278,13 +278,13 @@ def VGG_CIFAR10_spike(x_crop, y_, pretrained=False, end_with='fc1000', n_classes
 
          net = Quant_Conv2d(net, n_filter=512, filter_size=(3, 3), strides=(1, 1), padding='SAME', b_init=None, name='conv9')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn9')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
          spikes = spikes + tf.reduce_sum(net.outputs)*1*1*512
 
          net = Quant_Conv2d(net, n_filter=512, filter_size=(3, 3), strides=(1, 1), padding='VALID', b_init=None, name='conv10')
          net = BatchNormLayer(net, act=tf.nn.relu, is_train=is_train,  name='bn10')
-         net = Quant_Layer(net)
+         net = Quant_Layer(net, k=k, B=B)
 
 
 
